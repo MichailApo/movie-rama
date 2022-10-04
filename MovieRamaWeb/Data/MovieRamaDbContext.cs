4510bb5d60404ea138c28458a1df61396a82c8f4
Michael Apostolou <michalis.n.apostolou@gmail.com>
2022-10-04 20:01:18 +0300

    Initial Commit

---
+++ MovieRamaWeb/Data/MovieRamaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieRamaWeb.Data
+{
+    public class MovieRamaDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public DbSet<User> Users { get; set; }
+
+        public MovieRamaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
